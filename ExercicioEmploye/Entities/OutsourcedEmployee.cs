0dab67c92f16e9fceac509a50b6d3369e6f67c0b
Sammy Cristino <sammycristino@hotmail.com>
2022-05-11 18:38:50 -0300

    Add project files.

---
+++ ExercicioEmploye/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioEmploye.Entities
+{
+    internal class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge{ get; set; }
+
+        public OutsourcedEmployee()
+        {
+
+        }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour, double additionalCharge)
+            : base (name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+            return base.Payment() + (AdditionalCharge * 1.1);
+        }
+
+
+
+    }
+}
---
+++ ExercicioEmploye/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExercicioEmploye
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+        }
+    }
+}
